761b902be1532d2c345ba21460f8b1d1d12e3435
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2015-10-31 02:37:26 +0100

    Implemented yaw proportional controller

--- Software/imu_simple_test/imu_simple_test.ino
+++ Software/imu_simple_test/imu_simple_test.ino
@@ -11,11 +11,65 @@ const int MPU=0x68;  // I2C address of the MPU-6050
 
 int16_t GyZ;
 int16_t GyZoffset;
-float angle = 0;
+float angle_big = 0;
 
 unsigned long prev_ts;
 
-uint16_t iter;
+//uint16_t iter;
+
+
+
+#define LED_RED_PIN     13
+#define LED_GREEN_PIN   9
+#define BUZZER_PIN      8
+#define LINE_R_PIN      2
+#define LINE_M_PIN      3
+#define LINE_L_PIN      4
+#define M4A_PIN         5
+#define M3A_PIN         6
+#define BUTTON_PIN      7
+#define DIST_3_PIN      A3
+#define DIST_2_PIN      A2
+#define DIST_1_PIN      A1
+#define BATTERY_V_PIN   A0
+#define M1A_PIN         11
+#define M2A_PIN         10
+
+void init_motor_pins() {
+  pinMode(M1A_PIN,OUTPUT);
+  pinMode(M2A_PIN,OUTPUT);
+  pinMode(M3A_PIN,OUTPUT);
+  pinMode(M4A_PIN,OUTPUT);
+}
+
+void LmotorSpeed(int16_t vel) {
+  if(vel > 0) {
+    analogWrite(M1A_PIN, 0);
+    analogWrite(M2A_PIN, vel);
+  } else if(vel < 0) {
+    analogWrite(M1A_PIN, -vel);
+    analogWrite(M2A_PIN, 0);
+  } else {
+    analogWrite(M1A_PIN, 0);
+    analogWrite(M2A_PIN, 0);
+  }
+}
+
+void RmotorSpeed(int16_t vel) {
+  if(vel > 0) {
+    analogWrite(M3A_PIN, 0);
+    analogWrite(M4A_PIN, vel);
+  } else if(vel < 0) {
+    analogWrite(M3A_PIN, -vel);
+    analogWrite(M4A_PIN, 0);
+  } else {
+    analogWrite(M3A_PIN, 0);
+    analogWrite(M4A_PIN, 0);
+  }
+}
+
+
+
 
 void setup() {
   delay(400);
@@ -30,28 +84,26 @@ void setup() {
   Wire.write(0x6B);   // PWR_MGMT_1 register
   Wire.write(0);      // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-  Serial.begin(115200);
+  //Serial.begin(115200);
   delay(1000);
   GyZoffset = 0;
   for(int i=0; i<100; i++) {
     GyZoffset += readGyroRaw();
   }
   GyZoffset /= 100;
-  Serial.println("Offset:");
-  Serial.println(GyZoffset);
+  //Serial.println("Offset:");
+  //Serial.println(GyZoffset);
 
-  Serial.println("Gyro config register:");
+  //Serial.println("Gyro config register:");
   
   Wire.beginTransmission(MPU);
   Wire.write(0x1B);  // starting with register 0x47 (GYRO_CONFIG)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU,1,true);  // request 1 register
-  Serial.println(Wire.read());
-  
-  delay(10000);
+  //Serial.println(Wire.read());
 
   prev_ts = millis();
-  iter = 0;
+  //iter = 0;
 }
 
 int16_t readGyroRaw() {
@@ -69,11 +121,18 @@ void loop() {
 
   float dt = ((float)(ts-prev_ts))/1000000.;
   
-  angle += GyZ*dt;
-  if((iter % 100) == 0)
-    Serial.println(angle*2000./32768.); // +-2000 deg/s sensitivity; +-2^15 full scale
+  angle_big += GyZ*dt;
+  float angle = angle_big*2000./32768.;
+  int error = max(min(round(255.*angle/45.),255),-255);
+  //Serial.println(angle);
+
+  LmotorSpeed(50+error);
+  RmotorSpeed(50-error);
+  
+  //if((iter % 100) == 0)
+  //  Serial.println(angle*2000./32768.); // +-2000 deg/s sensitivity; +-2^15 full scale
   //delay(5000);
   prev_ts = ts;
-  iter++;
+  //iter++;
 }
 
